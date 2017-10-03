f42427aa8c6b0ee95a7063656265823b50663ccf
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-10-03 14:12:49 +1300

    add weights

--- Assets/LoadDat.cs
+++ Assets/LoadDat.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Linq;
 using System.Collections.Generic;
-using UnityEngine;
+using System;
 
 public class LoadDat : MonoBehaviour {
 
 	public int index = 0;
 	TextAsset[] structures;
+	TextAsset[] weights;
 	public GameObject spherePrefab;
-	public Material[] materials;
+	public GameObject markerPrefab;
+	private MaterialPropertyBlock[] materials;
 
 	void LoadFromDat()
 	{
@@ -25,8 +28,7 @@ public class LoadDat : MonoBehaviour {
 			var sphere = Instantiate(spherePrefab);
 			sphere.transform.parent = transform;
 			sphere.transform.localPosition = new Vector3(x, y, z);
-			sphere.transform.localScale = Vector3.one * 30;
-			sphere.GetComponent<Renderer>().material = materials[c];
+			sphere.GetComponent<Renderer>().SetPropertyBlock(materials[c]);
 		}
 	}
 
@@ -36,15 +38,60 @@ public class LoadDat : MonoBehaviour {
 		LoadFromDat();
 	}
 
+	public void LoadWeight(string name)
+	{
+		var match = "";
+		foreach (var w in weights)
+		{
+			if (w.name == name)
+			{
+				match = w.text;
+			}
+		}
+		if (match == "")
+		{
+			Debug.LogError(name + " is not a valid weight filename");
+			return;
+		}
+		var lines = match.Split('\n');
+		var ints = lines.Where(s => s != "").Select(s => Convert.ToInt16(s)).ToList();
+		Debug.Log("min: " + ints.Min() + ", max: " + ints.Max());
+		for (int i = 0; i < ints.Count; i++)
+		{
+			var sphere = transform.GetChild(i);
+			var w = ints[i];
+			if (w > 0)
+			{
+				var marker = Instantiate(markerPrefab);
+				marker.transform.parent = sphere;
+				marker.transform.localPosition = new Vector3(0, .5f, 0);
+				marker.transform.localScale = new Vector3(.1f, w / 10f, .1f);
+				marker.GetComponent<Renderer>().SetPropertyBlock(materials[3]);
+			}
+		}
+	}
+
+	void InitColors()
+	{
+		var red = new MaterialPropertyBlock();
+		red.SetColor("_Color", Color.red);
+		var green = new MaterialPropertyBlock();
+		green.SetColor("_Color", Color.green);
+		var blue = new MaterialPropertyBlock();
+		blue.SetColor("_Color", Color.blue);
+		var magenta = new MaterialPropertyBlock();
+		magenta.SetColor("_Color", Color.magenta);
+		materials = new MaterialPropertyBlock[] { red, green, blue, magenta };
+	}
+
 	// Use this for initialization
 	void Start () {
 		structures = Resources.LoadAll<TextAsset>("Structures/");
-		Debug.Log(structures.Length + " structures");
-		foreach (var s in structures)
-		{
-			Debug.Log(s.name);
-		}
+		weights = Resources.LoadAll<TextAsset>("Weights/");
+		InitColors();
+		Debug.Log(structures.Length + " structures " + weights.Length + " weights");
 		LoadFromDat();
+		LoadWeight("Weight_H3K4_sum_over_granule");
 	}
 	
 	// Update is called once per frame
